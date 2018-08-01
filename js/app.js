dcf98d3ed2deef628fb16d3898417e396fa1aedb
Amar <amarhafizuddeen@gmail.com>
2018-08-01 19:52:23 +0800

    Added bootstrap

--- js/app.js
+++ js/app.js
@@ -1,13 +1,25 @@
 $(document).ready(function() {
 	var $div = $('<div />').appendTo('body');
+	$div.addClass('container');
 
 	$.get('http://events.utm.my/json_event_future', function(data) {
 		/*optional stuff to do after success */
+		var $divRow = $('<div />').appendTo($div);
+		$divRow.addClass('row');
+
 		$.each(data, function(index, val) {
-			var $p = $('<p />').appendTo($div);
+			if ((index) % 3 == 0) {
+				$divRow = $('<div />').appendTo($div);
+				$divRow.addClass('row');
+			}
+
+			var $divEvent = $('<div />').appendTo($divRow);
+			$divEvent.addClass('event');
+
+			var $p = $('<p />').appendTo($divEvent);
 			$p.html(val.title);
 
-			$p = $('<img />').appendTo($div);
+			$p = $('<img />').appendTo($divEvent);
 			$p.attr('src', val.image);
 		});
 	});
